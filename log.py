2b6cbe51678643a736363502d05d5202cd76183b
Christopher Jansen <jansench@oregonstate.edu>
2021-03-03 16:14:37 -0800

    Update log.py
    small changes to not match substring process names

--- log.py
+++ log.py
@@ -12,14 +12,13 @@ def load_log(filename, process_check):
         line_info = line[:line.find("]: ")+2]
         line_log = line[line.find("]: ")+2:]
         line_info = line_info.split(" ")
-        # remove host name 
-        del line_info[3]
         # combine dates and times
         date_time = " ".join([line_info[i] for i in [0, 1, 2]])
         # process names could potentially contain spaces which is why we combine
-        # instead of just reading index 3
-        process_name = " ".join([line_info[i] for i in range(3, len(line_info))])
-        if(process_check in process_name):
+        # instead of just reading index 4
+        process_name = " ".join([line_info[i] for i in range(4, len(line_info))])
+        process_name = process_name.split("[")[0]
+        if(process_check == process_name):
             print(date_time, line_log, sep="", end="")
     print("\n")
 
